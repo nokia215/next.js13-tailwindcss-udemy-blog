ef753949f1f5cfc70571a3cbf0dacdc96498ec29
nokia215 <parttimework.panzer@gmail.com>
2023-09-30 12:58:53 +0900

    done: ch28

---
+++ src/app/articles/[id]/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+
+const Article = ({ params }: { params: { id: string } }) => {
+  //   console.log(params.id);
+  return (
+    <>
+      <div className="max-w-3xl mx-auto p-5">
+        <Image
+          src="https://source.unsplash.com/collection/1346951/1000x500?sig=2"
+          alt="image"
+          width={1280}
+          height={300}
+        ></Image>
+        <h1 className="text-4xl text-center mb-10 mt-10">
+          ここがタイトルです
+        </h1>
+        <div className="text-lg leading-relaxed text-justify">
+          <p>ここが本文です</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Article;
--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -17,14 +17,7 @@ const ArticleList = ({ articles }: ArticleListProps) => {
               <Link
                 href={`articles/${article.id}`}
                 className="hover:opacity-75"
-              >
-                <Image
-                  src="https://source.unsplash.com/collection/1346951/1000x500?sig=2"
-                  alt="image"
-                  width={1280}
-                  height={300}
-                ></Image>
-              </Link>
+              ></Link>
               <div className="bg-white flex flex-col justify-start p-6">
                 <Link href="#" className="text-blue-700 pb-4 font-bold">
                   Technology
