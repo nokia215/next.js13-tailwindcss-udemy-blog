3e639721e55abba2bd28fa8a6d3d1b9dac8399d0
nokia215 <parttimework.panzer@gmail.com>
2023-10-01 17:10:37 +0900

    done: ch34

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import { Article } from "./types";
+import { resolve } from "path";
 /**
  * ブログ記事を非同期に取得する関数です。
  * SSRに関する情報は以下を参照してください
@@ -66,4 +67,48 @@ export const getDetailArticle = async (id: string): Promise<Article> => {
 
   const article = await res.json();
   return article;
+};
+
+/**
+ * 新しいブログ記事を非同期に作成し、サーバーに送信します。
+ *
+ * @param {string} id - ブログ記事の一意のID
+ * @param {string} title - ブログ記事のタイトル
+ * @param {string} content - ブログ記事の内容
+ * @returns {Promise<Article>} - 作成されたブログ記事の情報を含むPromiseオブジェクト
+ * @throws {Error} - ブログ記事の作成中にエラーが発生した場合
+ *
+ * @example
+ * try {
+ *   const newArticleId = '123'; // 新しいブログ記事のID
+ *   const newArticleTitle = 'New Article'; // 新しいブログ記事のタイトル
+ *   const newArticleContent = 'This is the content of the new article.'; // 新しいブログ記事の内容
+ *   const createdArticle = await createArticle(newArticleId, newArticleTitle, newArticleContent);
+ *   console.log('新しいブログ記事が作成されました:', createdArticle);
+ * } catch (error) {
+ *   console.error('ブログ記事の作成中にエラーが発生しました:', error.message);
+ * }
+ */
+export const createArticle = async (
+  id: string,
+  title: string,
+  content: string
+): Promise<Article> => {
+  const currentDateTime = new Date().toISOString();
+  const res = await fetch(`https://localhost3001/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, title, content, currentDateTime }),
+  });
+
+  if (!res.ok) {
+    throw new Error("#createArticle ブログ記事の作成中にエラーが発生しました");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+
+  const newArticle = await res.json();
+  return newArticle;
 };
